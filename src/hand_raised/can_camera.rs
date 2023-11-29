5a1c97a0c19ad97b487120a0f4090c3d1ead96ab
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-11-28 19:07:56 -0800

    Use promise helper function

--- src/hand_raised/can_camera.rs
+++ src/hand_raised/can_camera.rs
@@ -1,12 +1,8 @@
+use self::use_promise::{use_promise, PromiseState};
 use js_sys::{Object, Reflect};
-use wasm_bindgen::JsValue;
-use wasm_bindgen_futures::{spawn_local, JsFuture};
-use wasm_react::{
-    clones, h,
-    hooks::{use_effect, use_state, Deps},
-    Component, VNode,
-};
-use web_sys::{console::log_1, window, MediaStreamConstraints};
+use wasm_react::{h, hooks::Deps, Component, VNode};
+use web_sys::{window, MediaStreamConstraints};
+mod use_promise;
 
 pub struct CanCamera {}
 
@@ -16,44 +12,26 @@ impl CanCamera {
     }
 }
 
-pub enum PromiseState {
-    NotStarted,
-    Pending,
-    Done(Result<JsValue, JsValue>),
-}
-
 impl Component for CanCamera {
     fn render(&self) -> VNode {
-        let resolved = use_state(|| PromiseState::NotStarted);
-        {
-            clones!(mut resolved);
-            use_effect(
-                move || {
-                    spawn_local(async move {
-                        resolved.set(|mut _resolved| PromiseState::Pending);
-                        let video_object = Object::new();
-                        Reflect::set(&video_object, &"facingMode".into(), &"user".into()).unwrap();
-                        let js_promise = window()
-                            .unwrap()
-                            .navigator()
-                            .media_devices()
-                            .unwrap()
-                            .get_user_media_with_constraints(
-                                &MediaStreamConstraints::new().video(&video_object),
-                            )
-                            .unwrap();
-                        let result = JsFuture::from(js_promise).await;
-                        match result {
-                            Ok(ref v) => log_1(&v),
-                            Err(ref v) => log_1(&v),
-                        }
-                        resolved.set(|_resolved| PromiseState::Done(result));
-                    });
-                },
-                Deps::none(),
-            );
-        }
-        let text = match *resolved.value() {
+        let promise = use_promise(
+            move || {
+                let video_object = Object::new();
+                Reflect::set(&video_object, &"facingMode".into(), &"user".into()).unwrap();
+                let js_promise = window()
+                    .unwrap()
+                    .navigator()
+                    .media_devices()
+                    .unwrap()
+                    .get_user_media_with_constraints(
+                        &MediaStreamConstraints::new().video(&video_object),
+                    )
+                    .unwrap();
+                js_promise
+            },
+            Deps::none(),
+        );
+        let text = match *promise.value() {
             PromiseState::NotStarted => "Will get camera",
             PromiseState::Pending => "Getting camera",
             PromiseState::Done(_) => "Got",
