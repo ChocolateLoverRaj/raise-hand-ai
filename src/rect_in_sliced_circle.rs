e36cb9fff0aa913d2af2c70f697ecbf03c1e3781
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-12-22 10:28:37 -0800

    5 tests passing for sliced circle rect

--- src/rect_in_sliced_circle.rs
+++ src/rect_in_sliced_circle.rs
@@ -1,5 +1,3 @@
-use real_float::Real;
-
 use crate::quadratic_root::quadratic_root;
 
 #[derive(PartialEq, Debug)]
@@ -18,78 +16,75 @@ pub struct Slice {
     pub direction: f64,
 }
 
+/// Returns the x coordinates of the intersections, least to greatest
+fn circle_line_intersection(m: f64, point: (f64, f64)) -> (f64, f64) {
+    let (x_0, y_0) = point;
+    let a = m.powi(2) + (1 as f64);
+    let b = (2 as f64) * y_0 * m - (2 as f64) * m.powi(2) * x_0;
+    let c = y_0.powi(2) - (2 as f64) * y_0 * m * x_0 + m.powi(2) * x_0.powi(2) - (1 as f64);
+    let (middle, difference) = quadratic_root(a, b, c);
+    (middle - difference, middle + difference)
+}
+
 pub fn rect_in_sliced_circle(
     ratio: Ratio,
     vertical_slice: Slice,
     horizontal_slice: Slice,
 ) -> Output {
-    let diagonal = (ratio.width * ratio.height).powf(0.5);
-    let with_horizontal_baseline = {
-        // Try to make a rectangle centered x
-        // For horizontal slice
-        // y + mx = sqrt(1 - x^2)
-        // (y + mx)^2 = 1 - x^2
-        // y^2 + 2ymx + m^2 * x^2 = 1 - x^2
-        // We get a quadratic equation
-        // (m^2 + 1)*x^2 + 2ymx + (y^2-1) = 0
+    // Try putting the center of the rectangle at the origin first
+    let m = ratio.height / ratio.width;
+    let a = m.powi(2) + (1 as f64);
+    let b = 0 as f64;
+    let c = -1 as f64;
+    let (middle, difference) = quadratic_root(a, b, c);
+    let right_x = middle + difference;
+    let scale = right_x / (ratio.width / (2 as f64));
+    let left_x = right_x - ratio.width * scale;
+    let top_y = ratio.height / (2 as f64) * scale;
+    let bottom_y = -top_y;
+
+    if !(horizontal_slice.direction * horizontal_slice.position.signum() > (0 as f64)
+        || horizontal_slice.position.abs() < top_y)
+    {
+        Output {
+            bottom_left_corner: (left_x, bottom_y),
+            scale,
+        }
+    } else {
+        let diagonal = (ratio.width * ratio.height).powf(0.5);
+        let m = ratio.height / (0.5 as f64) / ratio.width;
         let y_0 = horizontal_slice.position;
-        let final_x = {
-            let m = ratio.height / (0.5 * ratio.width);
-            let (middle, difference) = quadratic_root(
-                m.powi(2) + (1 as f64),
-                (2 as f64) * y_0 * m,
-                y_0.powi(2) - (1 as f64),
-            );
-            (middle + difference * horizontal_slice.direction).abs()
-        };
-        dbg!(final_x);
-        let final_x = final_x.min(((1 as f64) - y_0.powi(2)).sqrt());
-        dbg!(final_x);
-        if final_x < vertical_slice.position {
+        let a = m.powi(2) + (1 as f64);
+        let b = (2 as f64) * y_0 * m - (2 as f64) * m.powi(2) * (0 as f64);
+        let c = y_0.powi(2) - (2 as f64) * y_0 * m * (0 as f64) + m.powi(2) * (0 as f64).powi(2)
+            - (1 as f64);
+        let (middle, difference) = quadratic_root(a, b, c);
+        let x_intersect = middle + difference;
+
+        if vertical_slice.direction * vertical_slice.position.signum() > (0 as f64) {
+            let m = ratio.height / ratio.width;
+            let x_0 = vertical_slice.position;
+            let final_x = circle_line_intersection(m, (x_0, y_0)).1;
             Output {
-                bottom_left_corner: if horizontal_slice.direction == (1 as f64) {
-                    (0 as f64, y_0)
-                } else {
-                    (-final_x, -((1 as f64) - final_x.powi(2)).sqrt())
-                },
-                scale: final_x * (2 as f64) / ratio.width,
+                bottom_left_corner: (x_0, y_0),
+                scale: (final_x - x_0) / ratio.width,
+            }
+        } else if vertical_slice.position.abs() < x_intersect {
+            let m = -ratio.height / ratio.width;
+            let x_0 = vertical_slice.position;
+            let final_x = circle_line_intersection(m, (x_0, y_0)).0;
+            let scale = (x_0 - final_x) / ratio.width;
+            Output {
+                bottom_left_corner: (final_x, y_0),
+                scale,
             }
         } else {
-            if vertical_slice.direction == (1 as f64) {
-                let m = ratio.height / ratio.width;
-                let x_0 = vertical_slice.position * vertical_slice.direction;
-                let a = m.powi(2) + (1 as f64);
-                let b = (2 as f64) * y_0 * m - (2 as f64) * m.powi(2) * x_0;
-                let c =
-                    y_0.powi(2) - (2 as f64) * y_0 * m * x_0 + m.powi(2) * x_0.powi(2) - (1 as f64);
-                let (middle, difference) = quadratic_root(a, b, c);
-                let final_x = middle + difference;
-                Output {
-                    bottom_left_corner: (x_0, y_0),
-                    scale: (final_x - x_0) / ratio.width,
-                }
-            } else {
-                let m = -(ratio.height / ratio.width);
-                let x_0 = vertical_slice.position;
-
-                let a = m.powi(2) + (1 as f64);
-                let b = (2 as f64) * y_0 * m - (2 as f64) * m.powi(2) * x_0;
-                let c =
-                    y_0.powi(2) - (2 as f64) * y_0 * m * x_0 + m.powi(2) * x_0.powi(2) - (1 as f64);
-                let (middle, difference) = quadratic_root(a, b, c);
-                let final_x = middle - difference;
-                Output {
-                    bottom_left_corner: (final_x, y_0),
-                    scale: (x_0 - final_x) / ratio.width,
-                }
+            Output {
+                bottom_left_corner: (0 as f64, y_0),
+                scale: x_intersect / (ratio.width / (2 as f64)),
             }
         }
-    };
-    with_horizontal_baseline
-    // Output {
-    //     bottom_left_corner: (0.0, 0.0),
-    //     scale: 0.0,
-    // }
+    }
 }
 
 #[cfg(test)]
@@ -194,8 +189,8 @@ mod tests {
         assert_eq!(
             result,
             Output {
-                bottom_left_corner: (-0.8749140243641682, -0.48427827740968893),
-                scale: 0.10936425304552103
+                bottom_left_corner: (-0.8715755371245493, -0.49026123963255896),
+                scale: 0.10894694214056866
             }
         );
     }
@@ -205,23 +200,23 @@ mod tests {
         // https://www.desmos.com/calculator/8ywmkofj22
         let result = rect_in_sliced_circle(
             Ratio {
-                width: 32 as f64,
+                width: 16 as f64,
                 height: 9 as f64,
             },
             Slice {
-                position: -0.8,
-                direction: 1 as f64,
+                position: 1 as f64,
+                direction: -1 as f64,
             },
             Slice {
-                position: 1 as f64,
+                position: 0.5 as f64,
                 direction: -1 as f64,
             },
         );
         assert_eq!(
             result,
             Output {
-                bottom_left_corner: (0 as f64, -0.8),
-                scale: -0.034
+                bottom_left_corner: (-0.8715755371245493, -0.49026123963255896),
+                scale: 0.10894694214056866
             }
         );
     }
