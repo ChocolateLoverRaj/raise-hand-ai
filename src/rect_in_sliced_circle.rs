1696ba9d7d5296839a49d0e0f1d60e780113310e
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2023-12-23 16:24:58 -0800

    Better pointer rect
    Doesn't make you cross your pointer hand over your other hand, which can mess up the pose detection.
    
    The rect in sliced circle function has not been tested for every combination, so some bugs probably will be found for it later but for now it's ok.

--- src/rect_in_sliced_circle.rs
+++ src/rect_in_sliced_circle.rs
@@ -26,7 +26,7 @@ fn circle_line_intersection(m: f64, point: (f64, f64)) -> (f64, f64) {
     (middle - difference, middle + difference)
 }
 
-pub fn rect_in_sliced_circle(
+pub fn rect_in_sliced_unit_circle(
     ratio: Ratio,
     vertical_slice: Slice,
     horizontal_slice: Slice,
@@ -46,12 +46,30 @@ pub fn rect_in_sliced_circle(
     if !(horizontal_slice.direction * horizontal_slice.position.signum() > (0 as f64)
         || horizontal_slice.position.abs() < top_y)
     {
-        Output {
-            bottom_left_corner: (left_x, bottom_y),
-            scale,
+        if vertical_slice.position.abs() < right_x {
+            let m = (ratio.height / (2 as f64)) / ratio.width;
+            let solution = ((1 as f64) / (m.powi(2) + (1 as f64))).powf(0.5);
+            let scale = solution / ratio.width;
+            let bottom_y = -ratio.height / (2 as f64) * scale;
+
+            if vertical_slice.direction == 1 as f64 {
+                Output {
+                    bottom_left_corner: (0 as f64, bottom_y),
+                    scale,
+                }
+            } else {
+                Output {
+                    bottom_left_corner: (-solution, bottom_y),
+                    scale,
+                }
+            }
+        } else {
+            Output {
+                bottom_left_corner: (left_x, bottom_y),
+                scale,
+            }
         }
     } else {
-        let diagonal = (ratio.width * ratio.height).powf(0.5);
         let m = ratio.height / (0.5 as f64) / ratio.width;
         let y_0 = horizontal_slice.position;
         let a = m.powi(2) + (1 as f64);
@@ -87,14 +105,39 @@ pub fn rect_in_sliced_circle(
     }
 }
 
+pub fn rect_in_sliced_circle(
+    ratio: Ratio,
+    vertical_slice: Slice,
+    horizontal_slice: Slice,
+    position: (f64, f64),
+    radius: f64,
+) -> Output {
+    let vertical_slice = Slice {
+        position: (vertical_slice.position - position.0) / radius,
+        direction: vertical_slice.direction,
+    };
+    let horizontal_slice = Slice {
+        position: (horizontal_slice.position - position.1) / radius,
+        direction: horizontal_slice.direction,
+    };
+    let output = rect_in_sliced_unit_circle(ratio, vertical_slice, horizontal_slice);
+    Output {
+        bottom_left_corner: (
+            output.bottom_left_corner.0 * radius + position.0,
+            output.bottom_left_corner.1 * radius + position.1,
+        ),
+        scale: output.scale * radius,
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::rect_in_sliced_circle::{rect_in_sliced_circle, Output, Ratio, Slice};
+    use super::*;
 
     #[test]
     fn top_center_unrestricted() {
         // https://www.desmos.com/calculator/o2rt0jzjfh
-        let result = rect_in_sliced_circle(
+        let result = rect_in_sliced_unit_circle(
             Ratio {
                 width: 16 as f64,
                 height: 9 as f64,
@@ -120,7 +163,7 @@ mod tests {
     #[test]
     fn top_restricted_right_side() {
         // https://www.desmos.com/calculator/9h286kf4c0
-        let result = rect_in_sliced_circle(
+        let result = rect_in_sliced_unit_circle(
             Ratio {
                 width: 16 as f64,
                 height: 9 as f64,
@@ -146,7 +189,7 @@ mod tests {
     #[test]
     fn top_right() {
         //https://www.desmos.com/calculator/csdcd7shai
-        let result = rect_in_sliced_circle(
+        let result = rect_in_sliced_unit_circle(
             Ratio {
                 width: 16 as f64,
                 height: 9 as f64,
@@ -172,7 +215,7 @@ mod tests {
     #[test]
     fn huge_center_unrestricted() {
         // https://www.desmos.com/calculator/bjo9nejlts
-        let result = rect_in_sliced_circle(
+        let result = rect_in_sliced_unit_circle(
             Ratio {
                 width: 16 as f64,
                 height: 9 as f64,
@@ -198,7 +241,7 @@ mod tests {
     #[test]
     fn restricted_by_circle() {
         // https://www.desmos.com/calculator/8ywmkofj22
-        let result = rect_in_sliced_circle(
+        let result = rect_in_sliced_unit_circle(
             Ratio {
                 width: 16 as f64,
                 height: 9 as f64,
@@ -217,6 +260,111 @@ mod tests {
             Output {
                 bottom_left_corner: (-0.8715755371245493, -0.49026123963255896),
                 scale: 0.10894694214056866
+            }
+        );
+    }
+
+    #[test]
+    fn right_side() {
+        // https://www.desmos.com/calculator/yzkkhxpte6
+        let result = rect_in_sliced_unit_circle(
+            Ratio {
+                width: 16 as f64,
+                height: 9 as f64,
+            },
+            Slice {
+                position: 0 as f64,
+                direction: 1 as f64,
+            },
+            Slice {
+                position: -0.6 as f64,
+                direction: 1 as f64,
+            },
+        );
+        assert_eq!(
+            result,
+            Output {
+                bottom_left_corner: (0.0, -0.2707455769182841),
+                scale: 0.060165683759618685
+            }
+        );
+    }
+
+    #[test]
+    fn right_side_higher() {
+        // https://www.desmos.com/calculator/uhk23e0n7e
+        let result = rect_in_sliced_unit_circle(
+            Ratio {
+                width: 16 as f64,
+                height: 9 as f64,
+            },
+            Slice {
+                position: 0 as f64,
+                direction: 1 as f64,
+            },
+            Slice {
+                position: -0.2 as f64,
+                direction: 1 as f64,
+            },
+        );
+        assert_eq!(
+            result,
+            Output {
+                bottom_left_corner: (0.0, -0.2),
+                scale: 0.05898072429316711
+            }
+        );
+    }
+
+    #[test]
+    fn left_side() {
+        // https://www.desmos.com/calculator/hwgkjqeajd
+        let result = rect_in_sliced_unit_circle(
+            Ratio {
+                width: 16 as f64,
+                height: 9 as f64,
+            },
+            Slice {
+                position: 0 as f64,
+                direction: -1 as f64,
+            },
+            Slice {
+                position: -0.6 as f64,
+                direction: 1 as f64,
+            },
+        );
+        assert_eq!(
+            result,
+            Output {
+                bottom_left_corner: (-0.962650940153899, -0.2707455769182841),
+                scale: 0.060165683759618685
+            }
+        );
+    }
+
+    #[test]
+    fn scale_of_10() {
+        let result = rect_in_sliced_circle(
+            Ratio {
+                width: 16 as f64,
+                height: 9 as f64,
+            },
+            Slice {
+                position: 10 as f64,
+                direction: -1 as f64,
+            },
+            Slice {
+                position: 5 as f64,
+                direction: -1 as f64,
+            },
+            (0.0, 0.0),
+            10.0,
+        );
+        assert_eq!(
+            result,
+            Output {
+                bottom_left_corner: (-0.8715755371245493 * 10.0, -0.49026123963255896 * 10.0),
+                scale: 0.10894694214056866 * 10.0
             }
         );
     }
